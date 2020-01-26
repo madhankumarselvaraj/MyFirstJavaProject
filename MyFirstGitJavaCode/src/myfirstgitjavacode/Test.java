1a344066c68c0d7a9e24d51a8105809fed6a227e
madhankumarselvaraj <36690724+madhankumarselvaraj@users.noreply.github.com>
2020-01-26 12:06:25 +0530

    Test.java

---
+++ MyFirstGitJavaCode/src/myfirstgitjavacode/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        int a = 10, b = 9;
+        int c = a - b;
+        System.out.println("Subtraction : " + c);
+
+    }
+}
